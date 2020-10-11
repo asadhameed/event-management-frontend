1541d669f665553940e1a0825fe09a12029fe11c
Asad Hameed <asadhameed2002@yahoo.com>
2020-10-11 06:11:39 +0200

    install serve

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
  const api= Axios.create({
-     baseURL:'http://localhost:8000'
+     baseURL:'https://sport-event-api.herokuapp.com/'
      //baseURL:'http://192.168.10.3:8000'
  })
 
