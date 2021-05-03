6c0b3573522ff8215e20bf65478ad359ea22b8e0
Asad Hameed <asadhameed2002@yahoo.com>
2021-05-03 21:06:03 +0200

    Add github code link

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import './App.css';
-import Routers from './routers';
-import { Container } from 'reactstrap'
-import { ContextWrapper } from './user-context';
+import React from "react";
+import "./App.css";
+import Routers from "./routers";
+import { Container } from "reactstrap";
+import { ContextWrapper } from "./user-context";
 function App() {
   return (
-   
-      <Container> 
-         <ContextWrapper>
+    <Container>
+      <ContextWrapper>
         <h1>Event's App</h1>
-        <div className='content'>
+        <div className="github_link">
+          <a
+            rel="noopener noreferrer"
+            href="https://github.com/asadhameed/event-management-frontend"
+            target="_blank"
+          >
+            <h6>Front-End code</h6>
+          </a>
+          <a
+            rel="noopener noreferrer"
+            href="https://github.com/asadhameed/event-management-backend"
+            target="_blank"
+          >
+            <h6 style={{ float: "right" }}> Back-End code</h6>
+          </a>
+        </div>
+        <div className="content">
           <Routers />
         </div>
-        </ContextWrapper>
-      </Container>
-     
+      </ContextWrapper>
+    </Container>
   );
 }
 
